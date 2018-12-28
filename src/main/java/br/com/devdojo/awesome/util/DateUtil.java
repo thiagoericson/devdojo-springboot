77697545bf4339e0b8114aa7d3e3168bed614f1f
Thiago Ericson <ericson.thiago@gmail.com>
2018-12-28 12:05:27 -0200

    Spring Boot Essentials 02 - @Component, @Autowired e @SpringBootApplication

---
+++ src/main/java/br/com/devdojo/awesome/util/DateUtil.java
@@ -0,0 +1,14 @@
+package br.com.devdojo.awesome.util;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class DateUtil {
+    public String formatLocalDateTimeFormatToDatabaseStyle(LocalDateTime localDateTime){
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    }
+}
